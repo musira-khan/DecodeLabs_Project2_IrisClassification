a14252d41c8ef8b76d39117ad3e3785453819635
MUSIRA KHAN <musirakhan.here@gmail.com>
2026-08-10 01:46:03 +0500

    Update streamlitapi.py

--- streamlitapi.py
+++ streamlitapi.py
@@ -1,37 +1,57 @@
-import pickle 
+import pickle
 import streamlit as st
 import numpy as np
 
-scaler_file = pickle.load(open('scaler.pkl', 'rb')) 
+scaler_file = pickle.load(open('scaler.pkl', 'rb'))
 model_file = pickle.load(open('model.pkl', 'rb'))
 
+
 def pred_output(user_input):
-    scaled_input = scaler_file.transform(np.array(user_input).reshape(-1,4))
+    scaled_input = scaler_file.transform(
+        np.array(user_input).reshape(1, 4)
+    )
+
     ypred = model_file.predict(scaled_input)
+
     return ypred[0]
 
-def main(): 
+
+def main():
+
     st.title('Iris Classification')
-    st.text('Developed by - www.rubangino.in')
+    st.write('Predict the Iris flower class using Machine Learning.')
 
     st.divider()
 
-    # Input Variables
-    sepalLength = st.text_input('Enter the Sepal Length (Cm)')
-    sepalWidth = st.text_input('Enter the Sepal Width in (Cm)')
-    petalLength = st.text_input('Enter the Petal Length in (Cm)')
-    petalWidth = st.text_input('Enter the Petal Width in (Cm)')
+    sepalLength = st.number_input(
+        'Enter the Sepal Length (cm)'
+    )
 
-    if sepalLength.isalpha() or sepalWidth.isalpha() or petalLength.isalpha() or petalWidth.isalpha():
-        st.error("Input must be a Numeric!", icon='🚨')
-        st.error("It seems some given input was not a Numeric Value!!", icon='🤔')
+    sepalWidth = st.number_input(
+        'Enter the Sepal Width (cm)'
+    )
 
-    # Button to predict
+    petalLength = st.number_input(
+        'Enter the Petal Length (cm)'
+    )
+
+    petalWidth = st.number_input(
+        'Enter the Petal Width (cm)'
+    )
+
     if st.button('Predict'):
-        user_input = [sepalLength, sepalWidth, petalLength, petalWidth]
-        make_prediction = pred_output(user_input)
-        st.success(make_prediction)
-    
+
+        user_input = [
+            sepalLength,
+            sepalWidth,
+            petalLength,
+            petalWidth
+        ]
+
+        prediction = pred_output(user_input)
+
+        st.success(f'Predicted Class: {prediction}')
+
+
 if __name__ == '__main__':
     main()
-
